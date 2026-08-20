74931bdd6d81cace030aee07d730ac1da9a1cb18
thecontentgang <thecontentgang93@gmail.com>
2026-08-21 00:43:27 +0530

    Scroll TO TOp Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
 // Layout
@@ -10,16 +10,32 @@ import HomePage from './pages/Home/HomePage';
 import MenuPage from './pages/Menu/MenuPage';
 import CategoryPage from './pages/Menu/CategoryPage';
 import ProductDetailsPage from './pages/Product/ProductDetailsPage';
-import CartPage from './pages/Cartpage'
+import CartPage from './pages/Cartpage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 import SubscriptionPage from './pages/SubscriptionPage';
 import TestimonialsPage from './pages/TestimonialPage';
 
+// 1. Create the ScrollToTop component
+const ScrollToTop: React.FC = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // Scroll to the top-left corner of the window
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  // This component doesn't render anything visible
+  return null;
+};
+
 const App: React.FC = () => {
   return (
     <HelmetProvider>
       <Router>
+        {/* 2. Place it inside the Router, before your Routes */}
+        <ScrollToTop />
+        
         <Routes>
           {/* MainLayout wraps all these routes with Navbar, Footer, and CartDrawer */}
           <Route element={<MainLayout />}>
